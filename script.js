5ee7ce0920a8ada34a8188a47de78da2e726cc7a
Graham S <graham@sharmandigital.com>
2023-09-02 07:14:13 +0100

    edited code comments

--- script.js
+++ script.js
@@ -1,116 +1,78 @@
-console.log("testing");
-/*
-Pseudocode for Rock Paper Scissors in JavaScript:
-
-
-1. Initialize the game:
-    
-    a. Display game instructions to the user using console.log or alert.
-
-1.1 Get computer choice
-    Generate a random choice for the computer:
-    a. Create an array of choices: ["rock", "paper", "scissors"]
-    b. Randomly select an item from the choices array using Math.random().
-*/
+// Function to generate a random choice for the computer
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "scissors"];
-    const randomNumber = Math.floor(Math.random()*3);
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice());
 
+// Function to play a single round of the game
 function playRound(playerSelection, computerSelection) {
+    // Convert selections to lowercase for case-insensitive comparison
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    /*converts selections to lower case*/
 
-    if (playerSelection === computerSelection) {   
-        return `You chose ${playerSelection} and i chose ${computerSelection } so I'ts a tie!`;
-
-} else if (playerSelection === "rock") {
-    if (computerSelection === "scissors") {
-        return `You chose ${playerSelection} and i chose ${ computerSelection } so you win! Rock beats Scissors`;
-    } else {return `You chose ${playerSelection} and i chose ${computerSelection } so you loose! Paper beats Rock`;
-    }
-
-} else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-        return "you win! Paper beats Rock.";
-    } else {return "You loose! Scissors beats Paper.";
-}
-
-} else if (playerSelection === "scissors") {
-    if (computerSelection === "paper") {
-        return "You win! Scissors beats paper"
+    // Compare player's and computer's choices to determine the outcome
+    if (playerSelection === computerSelection) {
+        return `You chose ${playerSelection} and I chose ${computerSelection}. It's a tie!`;
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return `You chose ${playerSelection} and I chose ${computerSelection}. You win! Rock beats Scissors.`;
+        } else {
+            return `You chose ${playerSelection} and I chose ${computerSelection}. You lose! Paper beats Rock.`;
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock.";
+        } else {
+            return "You lose! Scissors beats Paper.";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beats Paper.";
+        } else {
+            return "You lose! Rock beats Scissors.";
+        }
     } else {
-        return "you loose! Rock beats Scissors";
-}
-} else {
-    return "Invalid selection. please choose Rock, paper, or scissors"
-}
+        return "Invalid selection. Please choose Rock, Paper, or Scissors.";
+    }
 }
 
-const playerSelection = prompt();
+// Get player's choice and a random computer choice
+const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+// Function to play a 5-round game and keep score
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-  
+
+    // Loop for 5 rounds
     for (let round = 1; round <= 5; round++) {
-      const playerSelection = prompt("Enter your choice (rock, paper, or scissors):");
-      const computerSelection = getComputerChoice();
-      
-      const roundResult = playRound(playerSelection, computerSelection);
-      console.log(roundResult);
-  
-      if (roundResult.includes("win")) {
-        playerScore++;
-      } else if (roundResult.includes("lose")) {
-        computerScore++;
-      }
+        const playerSelection = prompt(`Round ${round}: Enter your choice (Rock, Paper, or Scissors):`);
+        const computerSelection = getComputerChoice();
+
+        // Play a round and display the result
+        const roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+
+        // Update scores based on round result
+        if (roundResult.includes("win")) {
+            playerScore++;
+        } else if (roundResult.includes("lose")) {
+            computerScore++;
+        }
     }
-  
+
+    // Determine the winner of the game
     if (playerScore > computerScore) {
-      console.log("You win the game!");
+        console.log("You win the game!");
     } else if (playerScore < computerScore) {
-      console.log("Computer wins the game!");
+        console.log("Computer wins the game!");
     } else {
-      console.log("It's a tie! No winner.");
+        console.log("It's a tie! No winner.");
     }
-  }
-  
-  game();
+}
 
-  
-  
-  
-  
+// Call the game function to start the game
+game();
 
-
-/*
-2. Get the user's choice:
-    a. Use prompt() to ask the user to select "rock", "paper", or "scissors".
-    b. If the user's input is invalid (not one of the three choices), use prompt() again.
-
-
-
-4. Determine the winner:
-    a. If user's choice is the same as computer's choice:
-         - It's a tie! Use console.log or alert to inform the user.
-    b. Else if user's choice is "rock" and computer's choice is "scissors", OR
-         user's choice is "scissors" and computer's choice is "paper", OR
-         user's choice is "paper" and computer's choice is "rock":
-         - User wins! Use console.log or alert to inform the user.
-    c. Else:
-         - Computer wins! Use console.log or alert to inform the user.
-
-5. Display the results:
-    a. Use console.log or alert to show the user's choice, the computer's choice, and the result.
-
-6. Ask if the user wants to play again:
-    a. Use confirm() to ask if the user wants another round.
-    b. If yes, go back to step 2.
-    c. If no, end the game and say goodbye using console.log or alert.
-*/
