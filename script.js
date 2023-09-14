8dd8354bd9ee5631240eb6139f11a425d91112fb
Graham S <graham@sharmandigital.com>
2023-09-14 15:22:55 +0100

    feat: Implement core game logic for Rock-Paper-Scissors
    - Fix issue with 'Game Over' message not displaying.
    - Refactor game reset logic.
    - Set up event listeners for game control buttons.
    - Prepare for subsequent CSS styling and animations.

--- script.js
+++ script.js
@@ -1,78 +1,110 @@
-// Function to generate a random choice for the computer
+const MAX_SCORE = 5;
+
+// initialise scores
+let playerScore = 0;
+let computerScore = 0;
+// Function that randomly selects the computer's choice.
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-// Function to play a single round of the game
+// Function that compares player's choice against computer's to determine the winner of a round.
 function playRound(playerSelection, computerSelection) {
-    // Convert selections to lowercase for case-insensitive comparison
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    // Compare player's and computer's choices to determine the outcome
     if (playerSelection === computerSelection) {
-        return `You chose ${playerSelection} and I chose ${computerSelection}. It's a tie!`;
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            return `You chose ${playerSelection} and I chose ${computerSelection}. You win! Rock beats Scissors.`;
-        } else {
-            return `You chose ${playerSelection} and I chose ${computerSelection}. You lose! Paper beats Rock.`;
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return "You win! Paper beats Rock.";
-        } else {
-            return "You lose! Scissors beats Paper.";
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            return "You win! Scissors beats Paper.";
-        } else {
-            return "You lose! Rock beats Scissors.";
-        }
-    } else {
-        return "Invalid selection. Please choose Rock, Paper, or Scissors.";
+        return `It's a tie! Both chose ${playerSelection}.`;
+    }
+
+    switch (playerSelection) {
+        case "rock":
+            return computerSelection === "scissors" ? "You win! Rock beats Scissors." : "You lose! Paper beats Rock.";
+        case "paper":
+            return computerSelection === "rock" ? "You win! Paper beats Rock." : "You lose! Scissors beats Paper.";
+        case "scissors":
+            return computerSelection === "paper" ? "You win! Scissors beats Paper." : "You lose! Rock beats Scissors.";
+        default:
+            return "Invalid selection. Please choose Rock, Paper, or Scissors.";
     }
 }
 
-// Get player's choice and a random computer choice
-const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-const computerSelection = getComputerChoice();
+// Game helpers
+function announceOverallWinner() {
+    const gameResultDiv = document.getElementById("gameResult");
+    const playAgainButton = document.getElementById("playAgain");
+    if (playerScore === MAX_SCORE) {
+        gameResultDiv.innerHTML = "<strong>Game Over - You Won!</strong>";
+    } else if (computerScore === MAX_SCORE) {
+        gameResultDiv.innerHTML = "<strong>Game Over - You Lost!</strong>";
+    }
+        
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorButton.disabled = true;
+    
+        // Show the "Play Again" button
+        playAgainButton.style.display = "block";
+}
 
-// Function to play a 5-round game and keep score
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    
+     // Clear the game result and the results of the last round
+     document.getElementById("gameResult").textContent = ""; // This clears the "Game Over" message
+     document.getElementById("results").textContent = ""; 
+     // Update the DOM elements for scores:
+    document.getElementById("playerScore").textContent = playerScore;
+    document.getElementById("computerScore").textContent = computerScore;
+     // Hide the "Play Again" button
+    //  document.getElementById("playAgain").style.display = "none";
 
-    // Loop for 5 rounds
-    for (let round = 1; round <= 5; round++) {
-        const playerSelection = prompt(`Round ${round}: Enter your choice (Rock, Paper, or Scissors):`);
-        const computerSelection = getComputerChoice();
+     // Enable the game buttons
+   
+}
 
-        // Play a round and display the result
-        const roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
+// Function to handle a round when a button is clicked.
+function playGameRound(buttonChoice) {
+    const computerChoice = getComputerChoice();
+    const result = playRound(buttonChoice, computerChoice);
+    console.log(result);
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.textContent = result; // Updating the results div with the game result
 
-        // Update scores based on round result
-        if (roundResult.includes("win")) {
-            playerScore++;
-        } else if (roundResult.includes("lose")) {
-            computerScore++;
-        }
+    if (result.includes("win")) {
+        playerScore++;
+    } else if (result.includes("lose")) {
+        computerScore++;
     }
 
-    // Determine the winner of the game
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    } else if (playerScore < computerScore) {
-        console.log("Computer wins the game!");
-    } else {
-        console.log("It's a tie! No winner.");
+    // Update the DOM with the scores
+    document.getElementById("playerScore").textContent = playerScore;
+    document.getElementById("computerScore").textContent = computerScore;
+
+    // Check if someone won the game
+    if (playerScore === MAX_SCORE || computerScore === MAX_SCORE) {
+        announceOverallWinner();
     }
 }
 
-// Call the game function to start the game
-game();
+// Button references.
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorButton = document.getElementById("scissors");
 
+// Event listeners for game buttons.
+rockButton.addEventListener("click", () => playGameRound("rock"));
+paperButton.addEventListener("click", () => playGameRound("paper"));
+scissorButton.addEventListener("click", () => playGameRound("scissors"));
+
+//Event listners for Play Again
+document.getElementById("playAgain").addEventListener("click", function() {
+    resetGame();
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorButton.disabled = false;
+    this.style.display = "none";  // Hide the Play Again button
+});
