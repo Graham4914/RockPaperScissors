85c0b44ecfba0900606c4eaf8b101875dbfe99ea
Graham S <graham@sharmandigital.com>
2023-09-17 11:23:36 +0100

    Refactor and optimize Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-
 const MAX_SCORE = 5;
 
-// initialise scores
 let playerScore = 0;
 let computerScore = 0;
-// Function that randomly selects the computer's choice.
-function getComputerChoice() {
-    const startTime = performance.now();  // Start timer
+let totalThrows = 0;
 
+const iconMapping = {
+    rock: document.getElementById("rock").innerHTML,
+    paper: document.getElementById("paper").innerHTML,
+    scissors: document.getElementById("scissors").innerHTML
+};
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorButton = document.getElementById("scissors");
+
+function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
-    const computerChoice = choices[randomNumber];
-
-    const endTime = performance.now();  // End timer
-    const timeTaken = ((endTime - startTime)).toFixed(5); // Calculate time taken
-
-    return {
-        choice: computerChoice,
-        time: timeTaken
-    };
+    return { choice: choices[randomNumber] };
 }
 
-
-// Function that compares player's choice against computer's to determine the winner of a round.
 function playRound(playerSelection, computerSelection) {
+    let totalThrowsCounter = document.getElementById('totalThrows');
+    totalThrowsCounter.innerText = parseInt(totalThrowsCounter.innerText) + 1;
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection === computerSelection) {
-        return `It's a tie! Both chose ${playerSelection}.`;
-    }
+    if (playerSelection === computerSelection) return `It's a tie! Both chose ${playerSelection}.`;
 
     switch (playerSelection) {
         case "rock":
@@ -43,88 +41,90 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Announce winner
 function announceOverallWinner() {
     const gameResultDiv = document.getElementById("gameResult");
-    const playAgainButton = document.getElementById("playAgain");
+
     if (playerScore === MAX_SCORE) {
         gameResultDiv.innerHTML = "<strong>Game Over - You Won!</strong>";
     } else if (computerScore === MAX_SCORE) {
         gameResultDiv.innerHTML = "<strong>Game Over - You Lost!</strong>";
     }
-        
-        rockButton.disabled = true;
-        paperButton.disabled = true;
-        scissorButton.disabled = true;
-    
-        // Show the "Play Again" button
-        playAgainButton.style.display = "block";
+
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorButton.disabled = true;
+
+    document.getElementById("playAgain").style.display = "block";
 }
 
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
-    
-     // Clear the game result and the results of the last round
-     document.getElementById("gameResult").textContent = "";
-     document.getElementById("results").textContent = ""; 
-     // Update the DOM elements for scores:
+    totalThrows = 0;
+
+    document.getElementById("playerChoice").innerHTML = "";
+    document.getElementById("computerChoice").innerHTML = "";
+    document.getElementById("gameResult").textContent = "";
+    document.getElementById("results").textContent = ""; 
     document.getElementById("playerScore").textContent = playerScore;
     document.getElementById("computerScore").textContent = computerScore;
-     // Hide the "Play Again" button
-    //  document.getElementById("playAgain").style.display = "none";
-
-     // Enable the game buttons
-   
+    document.getElementById('totalThrows').innerText = totalThrows;
 }
 
-// Function to handle a round when a button is clicked.
 function playGameRound(buttonChoice) {
-    const computerData = getComputerChoice();
-    const computerChoice = computerData.choice;
-    const computerTime = computerData.time;
-    
+    const startScriptTime = performance.now();
+    const computerChoice = getComputerChoice().choice;
+
+    document.getElementById("playerChoice").innerHTML = iconMapping[buttonChoice.toLowerCase()];
+    document.getElementById("computerChoice").innerHTML = iconMapping[computerChoice.toLowerCase()];
+
     let result = playRound(buttonChoice, computerChoice);
+    const endScriptTime = performance.now();
+    const scriptExecutionTime = (endScriptTime - startScriptTime).toFixed(4);
+    
     if (result.includes("win")) {
-        result += ` I'm going to crash you into the fence next time!`;
+        result += ` I'm going to crash you next time!`;
         playerScore++;
     } else if (result.includes("lose")) {
-        result += ` You suck! How are your ribs? I beat you in less than ${computerTime} milliseconds!`;
+        result += ` It took me ${scriptExecutionTime} milliseconds to defeat you!`;
         computerScore++;
     } else {
-        result += ` You're only human. I made my choice in less than ${computerTime} milliseconds!`;
+        result += `You were lucky this time!  I made my choice in ${scriptExecutionTime} milliseconds!`;
     }
 
-    console.log(result);
     const resultsDiv = document.getElementById("results");
-    resultsDiv.textContent = result; // Updating the results div with the game result
-
+    resultsDiv.textContent = result;
    
-    // Update the DOM with the scores
     document.getElementById("playerScore").textContent = playerScore;
     document.getElementById("computerScore").textContent = computerScore;
 
-    // Check if someone won the game
     if (playerScore === MAX_SCORE || computerScore === MAX_SCORE) {
         announceOverallWinner();
     }
 }
 
-// Button references.
-const rockButton = document.getElementById("rock");
-const paperButton = document.getElementById("paper");
-const scissorButton = document.getElementById("scissors");
-
-// Event listeners for game buttons.
 rockButton.addEventListener("click", () => playGameRound("rock"));
 paperButton.addEventListener("click", () => playGameRound("paper"));
 scissorButton.addEventListener("click", () => playGameRound("scissors"));
 
-//Event listners for Play Again
+const buttons = document.querySelectorAll('.game-button');
+
+buttons.forEach(button => {
+    const delayClass = 'delay-' + (Math.floor(Math.random() * 3) + 1);
+    button.classList.add(delayClass);
+    button.addEventListener('click', () => {
+        button.classList.add('selected');
+        setTimeout(() => {
+            button.style.animation = '';
+            button.classList.remove('selected');
+        }, 2000); 
+    });
+});
+
 document.getElementById("playAgain").addEventListener("click", function() {
     resetGame();
     rockButton.disabled = false;
     paperButton.disabled = false;
     scissorButton.disabled = false;
-    this.style.display = "none";  // Hide the Play Again button
+    this.style.display = "none";
 });
